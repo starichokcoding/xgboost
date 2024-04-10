a78b6727c298e75626eb21cec5f61661b025f612
starichokcoding <166571862+starichokcoding@users.noreply.github.com>
2024-04-10 18:13:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.expander('About the app'):
 
 st.header('Input data')
 X,y = shap.datasets.iris()
-y = int(y)
+y = y.astype(int)
 X_display,y_display = shap.datasets.iris(display=True)
 
 with st.expander('About the data'):
