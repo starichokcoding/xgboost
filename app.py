f3cf57725a300d8462db57dba0efe0c9bf907f5b
starichokcoding <166571862+starichokcoding@users.noreply.github.com>
2024-04-10 17:48:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.expander('About the app'):
 
 st.header('Input data')
 X,y = shap.datasets.adult()
-X_display,y_display = shap.datasets.adult(display=True)
+X_display,y_display = shap.datasets.adult()
 
 with st.expander('About the data'):
     st.write('Adult census data is used as the example dataset.')
