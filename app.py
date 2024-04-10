3733faecd6cfcf41397dd3da8f3aac7cf1d23576
starichokcoding <166571862+starichokcoding@users.noreply.github.com>
2024-04-10 17:12:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from streamlit_shap import st_shap
-import shap
+#from streamlit_shap import st_shap
+#import shap
 from sklearn.model_selection import train_test_split
 import xgboost
 import numpy as np
